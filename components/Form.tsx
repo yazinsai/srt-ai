019914949a9761a1c2d9854c481b74f2c57693f7
Yazin <yazin@amalinvest.com>
2023-04-12 14:05:45 +0300

    Working form

---
+++ components/Form.tsx
@@ -0,0 +1,63 @@
+// src/components/SrtForm.tsx
+import React, { FormEvent, useState } from "react";
+
+interface Props {
+  onSubmit: (srtFile: File, language: string) => void;
+}
+
+const LANGUAGES = ["Arabic", "English", "Spanish", "French", "German"];
+
+const SrtForm: React.FC<Props> = ({ onSubmit }) => {
+  const [file, setFile] = useState<File>();
+  const [language, setLanguage] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (file && language) {
+      onSubmit(file, language);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center space-y-4"
+    >
+      <label htmlFor="srt-file" className="block font-bold text-xl">
+        SRT file:
+      </label>
+      <input
+        type="file"
+        id="srt-file"
+        accept=".srt"
+        onChange={(e) => setFile(e.target.files![0])}
+        className="bg-white py-2 px-4 border border-gray-300 rounded-lg"
+      />
+      <label htmlFor="language" className="block font-bold text-xl">
+        Language:
+      </label>
+      <select
+        id="language"
+        value={language}
+        onChange={(e) => setLanguage(e.target.value)}
+        className="bg-white py-2 px-4 border border-gray-300 rounded-lg"
+      >
+        <option value="">Select a language</option>
+        {LANGUAGES.map((lang, i) => (
+          <option key={i} value={lang}>
+            {lang}
+          </option>
+        ))}
+      </select>
+      <button
+        type="submit"
+        disabled={!file || !language}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-lg disabled:opacity-50"
+      >
+        Translate SRT &rarr;
+      </button>
+    </form>
+  );
+};
+
+export default SrtForm;
