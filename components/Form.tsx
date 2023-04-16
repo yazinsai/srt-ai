7742dce21d2f4a49b06d478dce71297ef8b36020
Yazin <yazin@amalinvest.com>
2023-04-16 15:32:03 +0300

    more langs

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,13 +1,19 @@
-// src/components/SrtForm.tsx
 import React, { FormEvent, useState } from "react";
 
 interface Props {
   onSubmit: (content: string, language: string) => void;
 }
 
-const LANGUAGES = ["Arabic", "English", "Spanish", "French", "German"];
+const LANGUAGES = [
+  "Arabic",
+  "English",
+  "French",
+  "German",
+  "Malay",
+  "Spanish",
+  "Turkish",
+];
 
-// src/utils/fileReader.ts
 const readFileContents = async (file: File): Promise<string> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -28,12 +34,25 @@ const readFileContents = async (file: File): Promise<string> => {
 const SrtForm: React.FC<Props> = ({ onSubmit }) => {
   const [file, setFile] = useState<File>();
   const [language, setLanguage] = useState<string>("");
+  const [dragging, setDragging] = useState<boolean>(false);
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
     if (file && language) {
       const content = await readFileContents(file);
       onSubmit(content, language);
+    }
+  };
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    setDragging(false);
+
+    if (e.dataTransfer.files && e.dataTransfer.files.length > 0) {
+      const droppedFile = e.dataTransfer.files[0];
+      if (droppedFile.type === "application/x-subrip") {
+        setFile(droppedFile);
+      }
     }
   };
 
@@ -45,13 +64,33 @@ const SrtForm: React.FC<Props> = ({ onSubmit }) => {
       <label htmlFor="srt-file" className="block font-bold text-xl">
         SRT file:
       </label>
-      <input
-        type="file"
+      <div
         id="srt-file"
-        accept=".srt"
-        onChange={(e) => setFile(e.target.files![0])}
-        className="bg-white py-2 px-4 border border-gray-300 rounded-lg"
-      />
+        onDragOver={(e) => {
+          e.preventDefault();
+          setDragging(true);
+        }}
+        onDragLeave={() => setDragging(false)}
+        onDrop={handleDrop}
+        className={`w-64 h-32 border-2 ${
+          dragging ? "border-blue-300" : "border-gray-300"
+        } rounded-lg flex items-center justify-center cursor-pointer`}
+      >
+        {file ? (
+          <p>{file.name}</p>
+        ) : (
+          <p className="text-center text-gray-500">
+            Drag and drop an SRT file here or click to select
+          </p>
+        )}
+        <input
+          type="file"
+          accept=".srt"
+          onChange={(e) => setFile(e.target.files![0])}
+          className="absolute opacity-0 cursor-pointer"
+          style={{ width: "100%", height: "100%" }}
+        />
+      </div>
       <label htmlFor="language" className="block font-bold text-xl">
         Language:
       </label>
@@ -69,7 +108,6 @@ const SrtForm: React.FC<Props> = ({ onSubmit }) => {
         ))}
       </select>
       <button
-        type="submit"
         disabled={!file || !language}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded-lg disabled:opacity-50"
       >
