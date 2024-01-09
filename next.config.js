43b7b2ad0ffb990b83d1ff0f3adbbd8dd6f2be93
Yazin <github-7itbcg5@yaz.in>
2024-01-09 11:34:31 +0300

    Fix tiktoken

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   env: {
     OPENAI_API_KEY: process.env.OPENAI_API_KEY
   },
+  webpack(config) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
