b7d6b349865f4890388ba7b58fca47e9c042b991
Yazin <github-7itbcg5@yaz.in>
2024-01-09 10:28:36 +0300

    Upgrade to NextJS 14

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 import { libre, roaldDahl } from "@/fonts";
 
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { FormEvent, useState } from "react";
 import Image from "next/image";
 
